ad605f654c41a1af32882ec537628505d0e5ee56
Marc Yang <m.y.yang001@gmail.com>
2014-12-31 19:16:02 -0500

    finally submission

--- html/main/app/resources/js/app.js
+++ html/main/app/resources/js/app.js
@@ -1,21 +1,32 @@
 angular.module('agFuture', [])
-        .controller('dataController', function ($scope, $http) {
-        	var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
+    .controller('dataController', function ($scope, $http) {
+        var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
 
-        	$scope.images =
-			        [{
-			        	  "photo": "http://nxworld.net/codepen/css-image-hover-effects/pic01.jpg",
-						  "name": "Green House 1",
-						  "location": "Boston",
-						  "products": ["Apples", "Pears", "oranges"]
-						}]
+        $scope.images = [
+            {
+                "name": "Apple Farm",
+                "location": "Boston",
+                "products": ["Lots of Apples", "Some Pears", "Some Oranges"],
+                "photo": "img/apples.jpeg"
+            }, {
+                "name": "Greenhouse of Leafy Greens",
+                "location": "Montreal",
+                "products": ["Kale", "Collard Greens", "Spinach", "Rapini"],
+                "photo": "img/leafygreen.jpg"
+            }, {
+                "name": "Orange Farm",
+                "location": "Orlando",
+                "products": ["Only Oranges"],
+                "photo": "img/oranges.jpeg"
+            }
+        ]
 
-/*
-			$http.jsonp(url)
-			    .success(function(data){
-			        console.log(data.posts[0]);
-			        $scope.dataresult = data.posts[0].content
-			    });
+        /*
+         $http.jsonp(url)
+         .success(function(data){
+         console.log(data.posts[0]);
+         $scope.dataresult = data.posts[0].content
+         });
 
-			    */
-        });
+         */
+    });
--- html/main/js/ngapp.js
+++ html/main/js/ngapp.js
@@ -4,79 +4,130 @@
 
 
 var farmApp = angular.module("farmApp", ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-farmApp.controller('ctrl', function ($scope, $http) {
-    $scope.isAnalyses = true;
-    $scope.isCtrl = false;
-    $scope.isLog = false;
-    $scope.showAnalyses = function () {
+farmApp.controller('ctrl', function ($scope, $http, $window) {
+
+
+    jQuery(function ($) {
+        var id = $("#fuck").text().substr(5, 1);
+        console.log(id);
+
+
         $scope.isAnalyses = true;
         $scope.isCtrl = false;
         $scope.isLog = false;
-    }
-    $scope.showCtrl = function () {
-        $scope.isAnalyses = false;
-        $scope.isCtrl = true;
-        $scope.isLog = false;
-    }
-    $scope.showLog = function () {
-        $scope.isAnalyses = false;
-        $scope.isCtrl = false;
-        $scope.isLog = true;
-    }
-    //var url = "http://127.0.0.1:3000/index?callback=JSON_CALLBACK"
-    var url_wind ="data/windpress.json?callback=JSON_CALLBACK";
-    //var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
-    $http({
-        method : "GET",
-        url : url_wind
-    }).then(function mySucces(response) {
-        resultList = response.data
-        resultList.map(function(obj){ 
-            
-            windData.push([obj.date, obj.windspeed])
-        })
-    }, function myError(response) {
-        console.log("error retrieving file: " + response)
-    });
-
-    //console.log("pass here");
-    
-    var url_temp ="data/temp.json?callback=JSON_CALLBACK";
-    //var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
-    $http({
-        method : "GET",
-        url : url_temp
-    }).then(function mySucces(response) {
-        
-        resultTempDailytList = response.data.daily;
-        resultTempMonthlyList = response.data.monthly;
-
-        resultTempDailytList.map(function(obj) {
-
-            var forcaststring = obj.date+":\n"+obj.low + " - "+obj.high+ " " + obj.forecast;
-
-            tempData.push([obj.date, obj.low, obj.low, obj.high, obj.high, forcaststring])
-
-            dailyRainData.push([obj.date, parseFloat(obj.precip)])
-
+        $scope.showAnalyses = function () {
+            $scope.isAnalyses = true;
+            $scope.isCtrl = false;
+            $scope.isLog = false;
+        }
+        $scope.showCtrl = function () {
+            $scope.isAnalyses = false;
+            $scope.isCtrl = true;
+            $scope.isLog = false;
+        }
+        $scope.showLog = function () {
+            $scope.isAnalyses = false;
+            $scope.isCtrl = false;
+            $scope.isLog = true;
+        }
+        //var url = "http://127.0.0.1:3000/index?callback=JSON_CALLBACK"
+        var url_wind = "data/windpress" + id + ".json?callback=JSON_CALLBACK";
+        var avgWind = 0;
+        //var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
+        $http({
+            method: "GET",
+            url: url_wind
+        }).then(function mySucces(response) {
+            resultList = response.data
+            resultList.map(function (obj) {
+                windData.push([obj.date, obj.windspeed])
+                avgWind += obj.windspeed;
+            })
+            avgWind = parseFloat(avgWind) / 10;
+            $scope.avgWindSpd = avgWind;
+        }, function myError(response) {
+            console.log("error retrieving file: " + response)
         });
 
-        
-        resultTempMonthlyList.map(function(obj) {
+        var url_temp = "data/temp" + id + ".json?callback=JSON_CALLBACK";
+        //var url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=JSON_CALLBACK";
+        $http({
+            method: "GET",
+            url: url_temp
+        }).then(function mySucces(response) {
 
-            monthlyRainData.push([obj.month.toString(), obj["total-precip"]])
-        })
-        
-    }, function myError(response) {
-        console.log(response)
+            resultTempDailytList = response.data.daily;
+            resultTempMonthlyList = response.data.monthly;
+
+            resultTempDailytList.map(function (obj) {
+
+                var forcaststring = obj.date + ":\n" + obj.low + " ~ " + obj.high + " " + obj.forecast;
+
+                tempData.push([obj.date, obj.low, obj.low, obj.high, obj.high, forcaststring])
+
+                dailyRainData.push([obj.date, parseFloat(obj.precip)])
+
+            });
+
+
+            resultTempMonthlyList.map(function (obj) {
+                monthlyRainData.push([obj.month.toString(), obj["total-precip"]])
+            })
+            resultTempDailytList.map(function (obj) {
+                avgTempData.push([obj.date, obj.avghigh, obj.high, obj.avglow, obj.low]);
+            });
+
+            // finding max/min
+            var maxTemp = -9999;
+            var dateOfMaxTemp;
+            var minTemp = 9999;
+            var dateOfMinTemp;
+            resultTempDailytList.map(function (obj) {
+                if (obj.high > maxTemp) {
+                    dateOfMaxTemp = obj.date;
+                    maxTemp = obj.high;
+                }
+                if (obj.low < minTemp) {
+                    dateOfMinTemp = obj.date;
+                    minTemp = obj.low;
+                }
+            });
+            $scope.highestTemp = maxTemp;
+            $scope.lowestTemp = minTemp;
+            $scope.maxTempDate = dateOfMaxTemp;
+
+            var maxRain = -9999;
+            var dateOfMaxRain = '1';
+            var minRain = 9999;
+            var dateOfMinRain = '';
+            var noRainRatio = 0;
+            resultTempDailytList.map(function (obj) {
+                if (obj.precip > maxRain) {
+                    dateOfMaxRain = obj.date;
+                    maxRain = obj.precip;
+                }
+                if (obj.precip < minRain) {
+                    dateOfMinRain = obj.date;
+                    minRain = obj.precip;
+                }
+                if (obj.precip <= 0.01) {
+                    noRainRatio += 1;
+                }
+            });
+            noRainRatio = parseFloat(noRainRatio) / 90;
+
+            $scope.maxRainDate = dateOfMaxRain;
+            $scope.percentageOfNoRain = noRainRatio;
+
+        }, function myError(response) {
+            console.log(response)
+        });
     });
-});
 
-farmApp.controller('ctrl2', function ($scope) {
     /* temperature control */
     var tempvalue = 28;
-    $scope.tempminus = function() {
-    
+    $scope.tempminus = function () {
+
         if (tempvalue == 0) {
             tempvalue = 0;
         }
@@ -88,7 +139,7 @@ farmApp.controller('ctrl2', function ($scope) {
     };
     $scope.tempminus();
 
-    $scope.tempadd = function() {
+    $scope.tempadd = function () {
 
         if (tempvalue == 100) {
             tempvalue = 100;
@@ -109,7 +160,7 @@ farmApp.controller('ctrl2', function ($scope) {
             humidity_value = 0;
         }
         else {
-            humidity_value = humidity_value-1;
+            humidity_value = humidity_value - 1;
         }
         $scope.humidity = humidity_value;
     };
@@ -122,7 +173,7 @@ farmApp.controller('ctrl2', function ($scope) {
             humidity_value = 100;
         }
         else {
-            humidity_value = humidity_value+1;
+            humidity_value = humidity_value + 1;
         }
         $scope.humidity = humidity_value;
     };
@@ -137,12 +188,12 @@ farmApp.controller('ctrl2', function ($scope) {
             oxygen_value = 0;
         }
         else {
-            oxygen_value = oxygen_value-1;
+            oxygen_value = oxygen_value - 1;
         }
         $scope.oxconcent = oxygen_value;
     };
     $scope.oxconcent_minus()
-    
+
 
     $scope.oxconcent_plus = function () {
 
@@ -150,7 +201,7 @@ farmApp.controller('ctrl2', function ($scope) {
             oxygen_value = 0;
         }
         else {
-            oxygen_value = oxygen_value+1;
+            oxygen_value = oxygen_value + 1;
         }
         $scope.oxconcent = oxygen_value;
     };
@@ -159,7 +210,7 @@ farmApp.controller('ctrl2', function ($scope) {
     /* illumination */
     var illu_value = 35.7
 
-    $scope.illu_minus = function() {
+    $scope.illu_minus = function () {
 
         if (illu_value == 0) {
             illu_value = 0;
@@ -172,7 +223,7 @@ farmApp.controller('ctrl2', function ($scope) {
     }
     $scope.illu_minus()
 
-    $scope.illu_plus = function() {
+    $scope.illu_plus = function () {
 
         if (illu_value == 100) {
             illu_value = 100;
@@ -183,7 +234,6 @@ farmApp.controller('ctrl2', function ($scope) {
         $scope.illumination = illu_value;
 
     }
-    
-    $scope.illu_plus()
 
+    $scope.illu_plus()
 });
